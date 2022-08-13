9b9623fc0b389c9f096f7873b486f388ef87fe24
Laila Ali <ms.laila.ali@outlook.com>
2022-08-13 18:25:53 -0500

    chore: update base styles and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Card from "./components/atoms/Card";
 import "./App.scss";
 import Heading from "./components/atoms/Heading";
+import Button from "components/atoms/Button";
+import {ReactComponent as Icon} from "./assets/svg/external-link.svg";
 
 function App() {
   return (
@@ -11,6 +13,13 @@ function App() {
         ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
         dis parturient montes, nascetur ridiculus mus. Donec quam felis,
       </p>
+      <Button
+        ariaLabel="button"
+        icon={<Icon />}
+        variant="tertiary"
+      >
+        Learn more
+      </Button>
     </Card>
   );
 }
